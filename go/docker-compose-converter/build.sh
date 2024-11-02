9042dc8b648302196063441854936de4f44ea0f3
Christian Burau <Christian.burau@icloud.com>
2024-11-02 05:40:23 +0100

    Update Skripte

--- go/docker-compose-converter/build.sh
+++ go/docker-compose-converter/build.sh
@@ -1,16 +1,7 @@
 #!/bin/bash
 
-# Create output directory
-#mkdir -p bin
-
-# Build for each platform
-#echo "Building for Linux..."
 GOOS=linux GOARCH=amd64 go build -o ./linux/docker-compose-converter main.go
-
-#echo "Building for Windows..."
 GOOS=windows GOARCH=amd64 go build -o ./windows/docker-compose-converter.exe main.go
+GOOS=darwin GOARCH=amd64 go build -o ./macos/amd64/docker-compose-converter main.go
+GOOS=darwin GOARCH=arm64 go build -o ./macos/arm64/docker-compose-converter main.go
 
-#echo "Building for macOS..."
-GOOS=darwin GOARCH=amd64 go build -o ./macos/docker-compose-converter main.go
-
-#echo "Builds completed. Check the 'bin' directory for executables."
